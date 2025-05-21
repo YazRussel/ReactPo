a379716de28b3b33e3b997fc3859574a10c8da0a
yazrussel <mcnp2021.12460@gmail.com>
2025-05-20 23:44:26 -1000

    Created a folder for the tutorial

---
+++ frontend-root-folder/last-week-tutorial/practicing/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./component/Heading";
+
+
+ReactDOM.render(
+  <Heading></Heading>,
+  document.getElementById("root")
+);
