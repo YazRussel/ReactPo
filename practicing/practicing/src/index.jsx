e62d541256775f0e90367f509423c1fbd81619fb
yazrussel <mcnp2021.12460@gmail.com>
2025-05-20 23:18:35 -1000

    Design for the for the component App.jsx and Card.jsx

--- practicing/practicing/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Heading from "./component/Heading";
-
-
-ReactDOM.render(
-  <Heading></Heading>,
-  document.getElementById("root")
-);
