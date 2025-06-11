33ad3d2aa485c226fe9cae25e7ed2cc9cbb25d0c
yazrussel <mcnp2021.12460@gmail.com>
2025-06-10 20:08:50 -1000

    Difference between Hooks and Class

---
+++ frontend-root-folder/front-end-fifth-week/312-class-components-vs-hooks/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
+//class
+class App extends React.Component {
+  render() {
+    return <FunctionalComponent />;
+  }
+}
+//hook
+// function App(){
+//   return <h1>Hello</h1>
+// }
+
+
+export default App;
